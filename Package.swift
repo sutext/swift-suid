1e5076cc4846863202ab2ee58b514b70709631af
supertext <supertext@icloud.com>
2021-04-23 21:30:27 +0800

    update pacakge name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "RUID",
+    name: "swift-ruid",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
